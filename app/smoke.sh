21f84dc7f7951e0f6babfb4324e78b63cbf1ded1
Zara Gebru <zgebru@thoughtworks.com>
2018-02-20 16:07:31 +0100

    run app for smoke-test

--- app/smoke.sh
+++ app/smoke.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-echo 'ip addresses'
+ls
+
+cd /bin/app
+
+npm install -g forever
+
+forever start app.js
+
 MY_ADDRESS=`hostname --ip-address`
+sleep 5
 
 curl http://${MY_ADDRESS}:3000
