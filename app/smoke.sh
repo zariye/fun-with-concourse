658b5f011f91857e697c0018ffc03f03a583f456
Zara Gebru <zgebru@thoughtworks.com>
2018-02-20 15:44:31 +0100

    run app through docker file

--- app/smoke.sh
+++ app/smoke.sh
@@ -2,18 +2,7 @@
 
 set -e
 
-ls
-
-cd /bin/app
-
-npm install
-npm build
-npm install -g forever
-
-forever start app.js
-
 echo 'ip addresses'
 MY_ADDRESS=`hostname --ip-address`
-sleep 5
 
 curl http://${MY_ADDRESS}:3000
